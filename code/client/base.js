1645486080516fc258cffd6a9d2ccc6853d64dca
likeran1 <31680590+likeran1@users.noreply.github.com>
2020-04-26 19:47:05 +0800

    修改全屏

--- code/client/base.js
+++ code/client/base.js
@@ -1,21 +1,36 @@
 (function () {
-    let btn = document.getElementById('btn');
-    let fullarea = document.getElementById('topo_canvas')
-    btn.addEventListener('click',function(){
-        if (fullarea.requestFullscreen) {
-            fullarea.requestFullscreen();
-          } else if (fullarea.webkitRequestFullScreen) {
-            fullarea.webkitRequestFullScreen();
-          } else if (fullarea.mozRequestFullScreen) {
-            fullarea.mozRequestFullScreen();
-          } else if (fullarea.msRequestFullscreen) {
-            // IE11
-            fullarea.msRequestFullscreen();
-          }
-        // Topology.init();
-      escType = false
-      fullscreen = true;
-    })
+    if(window.canvasNowId == "canvas0" || !window.canvasNowId){
+        let btn = document.getElementById('btn');
+        let fullarea = document.getElementById('topo_canvas')
+        btn.addEventListener('click',function(){
+            if (fullarea.requestFullscreen) {
+                fullarea.requestFullscreen();
+              } else if (fullarea.webkitRequestFullScreen) {
+                fullarea.webkitRequestFullScreen();
+              } else if (fullarea.mozRequestFullScreen) {
+                fullarea.mozRequestFullScreen();
+              } else if (fullarea.msRequestFullscreen) {
+                // IE11
+                fullarea.msRequestFullscreen();
+              }
+        })
+    }else{
+        let btn = window.parent.document.getElementById('btn');
+        let fullarea = parent.$('#'+window.top.canvasNowId).contents().find("#topo_canvas")
+        btn.addEventListener('click',function(){
+            if (fullarea.requestFullscreen) {
+                fullarea.requestFullscreen();
+              } else if (fullarea.webkitRequestFullScreen) {
+                fullarea.webkitRequestFullScreen();
+              } else if (fullarea.mozRequestFullScreen) {
+                fullarea.mozRequestFullScreen();
+              } else if (fullarea.msRequestFullscreen) {
+                // IE11
+                fullarea.msRequestFullscreen();
+              }
+        })
+    }
+
 
 })();
 
